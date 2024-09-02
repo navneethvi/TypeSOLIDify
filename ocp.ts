682f71959f70b8a66c72d178f13a69816ef9d854
Navaneeth V <navaneethvinod18@gmail.com>
2024-09-02 21:43:08 +0530

    Documented SRP and OCP with more clarity and definitions

---
+++ ocp.ts
@@ -0,0 +1,83 @@
+import { logger } from "@envy-core/common";
+
+//* The following code demonstrates an example of adhering to the Single Responsibility Principle (SRP) and separation of concerns.
+//* The SRP states that a class should have only one reason to change, meaning it should have only one job.
+//* This code also separates the responsibilities of fetching, displaying, and managing user data into different classes.
+
+//? Interface defining a User
+
+interface User {
+  name: string;
+  email: string;
+}
+
+//? Service class for fetching and saving user data
+class UserService {
+  //* Simulate fetching user data from an API.
+  async fetchUserData(): Promise<User> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          name: "Navaneeth Vinod",
+          email: "navaneethvinod27@gmail.com",
+        });
+      });
+    });
+  }
+
+  //* Simulate saving user data to a database.
+  async saveUserData(user: User): Promise<void> {
+    logger.info("Saving user to the database...");
+    console.log(user);
+
+    //* Logic to save the user to the database can be implemented here.
+  }
+}
+
+//? Class responsible for displaying user information
+//* The UserDisplay class handles the presentation logic for user data.
+class UserDisplay {
+  // Log the user's name and email.
+  displayUser(user: User): void {
+    logger.info(`Name ===> ${user.name}`);
+    logger.info(`Email ===> ${user.email}`);
+  }
+}
+
+//? Class responsible for managing user operations
+//* The ManageUser class coordinates the operations of fetching, displaying, and saving user data.
+class ManageUser {
+  private userService: UserService;
+  private userDisplay: UserDisplay;
+
+  //* Constructor takes instances of UserService and UserDisplay, promoting dependency injection and testability.
+  constructor(userService: UserService, userDisplay: UserDisplay) {
+    this.userService = userService;
+    this.userDisplay = userDisplay;
+  }
+
+  async initializeUser(): Promise<void> {
+    const user = await this.userService.fetchUserData();
+    this.userDisplay.displayUser(user);
+  }
+
+  async saveUser(user: User): Promise<void> {
+    await this.userService.saveUserData(user);
+  }
+}
+
+//? Example usage of the UserService, UserDisplay, and ManageUser classes.
+
+(async () => {
+  const userService = new UserService();
+  const userDisplay = new UserDisplay();
+  const manageUser = new ManageUser(userService, userDisplay);
+
+  await manageUser.initializeUser();
+
+  const user: User = {
+    name: "Navaneeth Vinod",
+    email: "navaneethvinod27@gmail.com",
+  };
+  await manageUser.saveUser(user);
+})();
